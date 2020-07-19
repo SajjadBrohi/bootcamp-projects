e5a43faddb5a65f27b308d51f3ee1354d2a894cf
Sajjad Brohi <s.brohi@uq.net.au>
2020-07-19 23:29:29 +1000

    dicee project

--- Sec12 - dicee/script.js
+++ Sec12 - dicee/script.js
@@ -1,8 +1,23 @@
-const playerOneDiceNumber = Math.floor(Math.random() * 6) + 1;
-const playerTwoDiceNumber = Math.floor(Math.random() * 6) + 1;
+function gameOfDice() {
+	const playerOneDiceNumber = Math.floor(Math.random() * 6) + 1;
+	const playerTwoDiceNumber = Math.floor(Math.random() * 6) + 1;
+	const containerHeading = document.querySelector('.container h1');
 
-if (playerOneDiceNumber > playerTwoDiceNumber) {
-	document.querySelector('.container h1').innerHTML = '🚩 Player 1 Wins!';
-} else {
-	document.querySelector('.container h1').innerHTML = '🚩 Player 2 Wins!';
+	if (playerOneDiceNumber > playerTwoDiceNumber) {
+		containerHeading.innerHTML = '🚩 Player 1 Wins!';
+	} else if (playerOneDiceNumber === playerTwoDiceNumber) {
+		containerHeading.innerHTML = 'Draw!';
+	} else {
+		containerHeading.innerHTML = 'Player 2 Wins! 🚩';
+	}
+
+	document
+		.querySelector('.img1')
+		.setAttribute('src', 'images/dice' + playerOneDiceNumber + '.png');
+
+	document
+		.querySelector('.img2')
+		.setAttribute('src', 'images/dice' + playerTwoDiceNumber + '.png');
 }
+
+window.onbeforeunload = gameOfDice();
