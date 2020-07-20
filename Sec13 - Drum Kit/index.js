7103deee5a3dcfad8a34ac15ca63f37069b31115
Sajjad Brohi <s.brohi@uq.net.au>
2020-07-21 00:52:00 +1000

    Drumkit project

--- Sec13 - Drum Kit/index.js
+++ Sec13 - Drum Kit/index.js
@@ -1,16 +1,43 @@
 const drumSet = document.querySelectorAll('.set button');
 
 function playSound(key) {
+	var drum = '';
+
 	switch (key) {
 		case 'a':
-			const drum = new Audio('sounds/crash.mp3');
-			drum.play();
+			drum = new Audio('sounds/kick-bass.mp3');
+			break;
+
+		case 'w':
+			drum = new Audio('sounds/crash.mp3');
+			break;
+
+		case 's':
+			drum = new Audio('sounds/snare.mp3');
+			break;
+
+		case 'd':
+			drum = new Audio('sounds/tom-1.mp3');
+			break;
+
+		case 'j':
+			drum = new Audio('sounds/tom-2.mp3');
+			break;
+
+		case 'k':
+			drum = new Audio('sounds/tom-3.mp3');
+			break;
+
+		case 'l':
+			drum = new Audio('sounds/tom-4.mp3');
 			break;
 
 		default:
 			console.log('default case');
 			break;
 	}
+
+	drum.play();
 }
 
 drumSet.forEach((drum) =>
