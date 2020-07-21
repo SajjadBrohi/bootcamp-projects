252b8ba8a58c8f984bc2d06b18ea5b7a01e558cc
Sajjad Brohi <s.brohi@uq.net.au>
2020-07-21 19:16:57 +1000

    Simon game project

--- Sec15 - Simon Game/index.js
+++ Sec15 - Simon Game/index.js
@@ -1,1 +1,15 @@
-console.log('here');
+const levelTitle = $('#level-title');
+const levelNumber = 1;
+
+function changeLevelTitle() {
+	levelTitle.text('Level ' + levelNumber);
+}
+
+function startGame() {
+	//stub
+}
+
+$('body').on('keypress', () => {
+	changeLevelTitle();
+	startGame();
+});
