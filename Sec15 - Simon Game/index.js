3ed4f18c61365ede300001992b774343cd9f356b
Sajjad Brohi <s.brohi@uq.net.au>
2020-07-21 19:44:28 +1000

    Simon game project

--- Sec15 - Simon Game/index.js
+++ Sec15 - Simon Game/index.js
@@ -1,12 +1,35 @@
 const levelTitle = $('#level-title');
 const levelNumber = 1;
+const buttons = $('.btn');
+const buttonsPressed = [];
+
+console.log(buttons);
+
+function buttonDisplayAndSOund(buttonNumber) {
+	const buttonColor = buttons[buttonNumber].getAttribute('id');
+	const buttonSound = new Audio('sounds/' + buttonColor + '.mp3');
+	console.log(buttonColor);
+	buttonSound.play();
+
+	buttons[buttonNumber].classList.add('computer-press');
+	setInterval(
+		() => buttons[buttonNumber].classList.remove('computer-press'),
+		100,
+	);
+}
+
+function computerButtonPress() {
+	const buttonNumber = Math.floor(Math.random() * 4);
+	buttonsPressed.push(buttonNumber);
+	buttonDisplayAndSOund(buttonNumber);
+}
 
 function changeLevelTitle() {
 	levelTitle.text('Level ' + levelNumber);
 }
 
 function startGame() {
-	//stub
+	computerButtonPress();
 }
 
 $('body').on('keypress', () => {
