04665b638f617a4839e17e430e4103a457881984
Sajjad Brohi <s.brohi@uq.net.au>
2020-07-21 20:39:24 +1000

    Simon game project

--- Sec15 - Simon Game/index.js
+++ Sec15 - Simon Game/index.js
@@ -1,27 +1,67 @@
 const levelTitle = $('#level-title');
-const levelNumber = 1;
 const buttons = $('.btn');
-const buttonsPressed = [];
+const computerButtonsPressed = [];
+const playerButtonsPressed = [];
+let levelNumber = 1;
 
 console.log(buttons);
 
-function buttonDisplayAndSOund(buttonNumber) {
+function gameOver() {
+	$('body').addClass('.game-over');
+	setInterval(() => $('body').removeClass('.game-over'), 500);
+	levelTitle.text('Game Over, Press Any Key to Restart');
+	levelNumber = 1;
+	computerButtonsPressed.length = 0;
+	playerButtonsPressed.length = 0;
+}
+
+function getIndexOfColor(color) {
+	let indexNumber = 0;
+
+	switch (color) {
+		case 'green':
+			indexNumber = 0;
+			break;
+		case 'red':
+			indexNumber = 1;
+			break;
+		case 'yellow':
+			indexNumber = 2;
+			break;
+		case 'blue':
+			indexNumber = 3;
+			break;
+		default:
+			console.log('Default case');
+			break;
+	}
+
+	return indexNumber;
+}
+
+function checkClickedNumber(button) {
+	const buttonNumber = getIndexOfColor(button);
+	playerButtonsPressed.push(buttonNumber);
+	const indexOfCurrentButton = playerButtonsPressed.indexOf(buttonNumber);
+	return (
+		computerButtonsPressed[indexOfCurrentButton] === buttonNumber &&
+		computerButtonsPressed.length === playerButtonsPressed.length
+	);
+}
+
+function buttonDisplayAndSOund(buttonNumber, classChange) {
 	const buttonColor = buttons[buttonNumber].getAttribute('id');
 	const buttonSound = new Audio('sounds/' + buttonColor + '.mp3');
-	console.log(buttonColor);
 	buttonSound.play();
 
-	buttons[buttonNumber].classList.add('computer-press');
-	setInterval(
-		() => buttons[buttonNumber].classList.remove('computer-press'),
-		100,
-	);
+	buttons[buttonNumber].classList.add(classChange);
+	setInterval(() => buttons[buttonNumber].classList.remove(classChange), 100);
 }
 
 function computerButtonPress() {
 	const buttonNumber = Math.floor(Math.random() * 4);
-	buttonsPressed.push(buttonNumber);
-	buttonDisplayAndSOund(buttonNumber);
+	computerButtonsPressed.push(buttonNumber);
+	buttonDisplayAndSOund(buttonNumber, 'computer-press');
 }
 
 function changeLevelTitle() {
@@ -30,9 +70,23 @@ function changeLevelTitle() {
 
 function startGame() {
 	computerButtonPress();
+	changeLevelTitle();
 }
 
 $('body').on('keypress', () => {
-	changeLevelTitle();
 	startGame();
+});
+
+buttons.on('click', (event) => {
+	const clickedElementColor = event.target.id;
+	buttonDisplayAndSOund(getIndexOfColor(clickedElementColor), 'pressed');
+
+	if (checkClickedNumber(clickedElementColor)) {
+		console.log('all elements clicked; new round');
+		startGame();
+	} else if (!checkClickedNumber(clickedElementColor)) {
+		console.log(computerButtonsPressed);
+		console.log(playerButtonsPressed);
+		gameOver();
+	}
 });
