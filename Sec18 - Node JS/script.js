47edab62e13a82bfc5507a91a192af899cbabb91
Sajjad Brohi <s.brohi@uq.net.au>
2020-07-22 18:10:48 +1000

    Node JS project

--- Sec18 - Node JS/script.js
+++ Sec18 - Node JS/script.js
@@ -1,3 +1,33 @@
-'use strict';
+// 'use strict';
 
-console.log(document.querySelector('.last').outerHTML);
+function hello() {
+	console.log(this);
+}
+
+hello();
+
+() => console.log(this);
+
+const pin = function () {
+	console.log(this); //undefined
+	function pinner() {
+		console.log(this);
+	}
+	console.log('pinner next');
+	pinner(); //undefined
+};
+
+console.log('pin next');
+pin();
+
+const pin2 = () => {
+	console.log(this);
+};
+
+const pin3 = () => console.log(this);
+
+console.log('pin2 next');
+pin2();
+
+console.log('pin3 next');
+pin3();
