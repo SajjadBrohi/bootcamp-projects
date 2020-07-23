e54bd84719a691b9ce92a51535f36edf9892dcc0
SajjadBrohi <s.brohi@uq.net.au>
2020-07-23 18:44:34 +1000

    New routes

--- Sec19 my-express-server/server.js
+++ Sec19 my-express-server/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4200;
 
 app.get("/", function (req, res) {
   res.send("hello man");
 })
+
+app.get("/contact", (req, res) => {
+  res.send("You can contact me whereever you want.");
+})
+
+app.get("/about", (req, res) => res.send("Elo, my name is Jay!"));
+
+
 app.listen(port, () => console.log(`hello`));
