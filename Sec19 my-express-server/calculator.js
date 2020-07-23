723e2f8cbb32d1df2b8d3dfadcf20422fa705cf3
SajjadBrohi <s.brohi@uq.net.au>
2020-07-23 20:15:02 +1000

    Calculator localhost server

--- Sec19 my-express-server/calculator.js
+++ Sec19 my-express-server/calculator.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/about", (req, res) => res.send("please stop"));
+
+app.post("/", (req, res) => {
+
+  const num1 = parseInt(req.body.num1);
+  const num2 = parseInt(req.body.num2);
+
+  res.send("Your results are " + (num1 + num2));
+})
 
 app.listen(3000, () => console.log("Server started at port 3000."));
