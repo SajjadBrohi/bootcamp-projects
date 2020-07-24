89f572addade4aa669839f3a9270ae1df856e4f4
SajjadBrohi <s.brohi@uq.net.au>
2020-07-24 22:54:06 +1000

    Project set up

--- Sec20 - API/mail-subscription-app/app.js
+++ Sec20 - API/mail-subscription-app/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000, () => console.log('Server running at port 3000'));
