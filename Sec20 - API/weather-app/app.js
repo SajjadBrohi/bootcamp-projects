7402f9dd2bbe8b289771f331ae0d3dda3f804de1
SajjadBrohi <s.brohi@uq.net.au>
2020-07-24 20:08:41 +1000

    Weather api completed

--- Sec20 - API/weather-app/app.js
+++ Sec20 - API/weather-app/app.js
@@ -3,26 +3,36 @@ const https = require('https');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  const url = "https://samples.openweathermap.org/data/2.5/weather?q=khuzdar&appid=439d4b804bc8187953eb36d2a8c26a02";
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+  const cityName = req.body.cityName;
+
+  console.log(cityName);
+  const apiKey = "9f5922f82d0ba2ed132cc16c148b8caf"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=metric";
   https.get(url, (response) => {
-    console.log(response.statusCode)
+    // console.log(response.statusCode)
     response.on('data', (data) => {
       const weatherData = JSON.parse(data);
+
+      // console.log(weatherData);
+
+
       const temp = weatherData.main.temp;
       const tempDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      console.log(tempDescription);
-      res.write("<h1>The weather in Khuzdar is " + temp + "</h1>\
-      <h1>The weather in Khuzdar is like " + tempDescription + "</h1> ");
+
+      res.write("<h1>The weather in " + cityName + " is " + temp + "</h1>\
+      <h1>The weather in " + cityName + " is like " + tempDescription + "</h1> ");
       res.write("<img src=http://openweathermap.org/img/wn/" + icon + "@2x.png>");
       res.send();
     })
   });
-});
-
-app.post('/', (req, res) => {
-
 })
 
 app.listen(3000, () => console.log('Server is running on port 3000.'));
