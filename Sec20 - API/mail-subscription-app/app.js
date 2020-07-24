0691e2c44c94660772a06340c140506a117485ed
SajjadBrohi <s.brohi@uq.net.au>
2020-07-24 22:00:23 +1000

    New package

--- Sec20 - API/mail-subscription-app/app.js
+++ Sec20 - API/mail-subscription-app/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
 app.listen(3000, () => console.log('Server running at port 3000'));
