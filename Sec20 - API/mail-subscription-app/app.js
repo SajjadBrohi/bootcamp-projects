390e78d17d8d917d7556935b62ea0da072178b41
SajjadBrohi <s.brohi@uq.net.au>
2020-07-25 14:59:18 +1000

    Final project completion

--- Sec20 - API/mail-subscription-app/app.js
+++ Sec20 - API/mail-subscription-app/app.js
@@ -70,4 +70,4 @@ app.post('/failure', (req, res) => {
   res.redirect('/');
 })
 
-app.listen(3000, () => console.log('Server running at port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server running at port 3000'));
