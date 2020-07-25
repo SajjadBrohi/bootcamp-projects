db959a86ac16c97740d5c4156da7b773c67971ba
SajjadBrohi <s.brohi@uq.net.au>
2020-07-25 19:39:27 +1000

    Changed from HTTP get to HTTP request

--- Sec20 - API/weather-app/app.js
+++ Sec20 - API/weather-app/app.js
@@ -14,20 +14,29 @@ app.post('/', (req, res) => {
   const apiKey = "9f5922f82d0ba2ed132cc16c148b8caf"
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=metric";
 
-  https.get(url, (response) => {
-    // console.log(response.statusCode)
+  const options = {
+    method: "GET",
+    auth: url
+  }
+
+  const request = https.request(url, options, (response) => {
+
+    console.log(response.statusCode)
     response.on('data', (data) => {
       const weatherData = JSON.parse(data);
+      // console.log(weatherData);
       const temp = weatherData.main.temp;
       const tempDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
 
       res.write("<h1>The weather in " + cityName + " is " + temp + "</h1>\
-      <h1>The weather in " + cityName + " is like " + tempDescription + "</h1> ");
+        <h1>The weather in " + cityName + " is like " + tempDescription + "</h1> ");
       res.write("<img src=http://openweathermap.org/img/wn/" + icon + "@2x.png>");
       res.send();
     })
   });
+
+  request.end();
 })
 
 app.listen(3000, () => console.log('Server is running on port 3000.'));
