e214c426c7e865ac58650e8dee5ee8788308bbaa
SajjadBrohi <s.brohi@uq.net.au>
2020-08-06 18:36:00 +1000

    Add environment variables to hide API keys

--- Sec20 - API/mail-subscription-app/app.js
+++ Sec20 - API/mail-subscription-app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const https = require('https');
 const { request } = require('http');
+require('dotenv').config();
 
 const app = express();
 
@@ -8,66 +9,56 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/signup.html');
+	res.sendFile(__dirname + '/signup.html');
 });
 
 app.post('/', (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const email = req.body.email;
 
-  // We change the data to an Object to be changed to Stringified JSON because
-  // that is the data that the Mailchimp API requires as seen from examples
+	const data = {
+		members: [
+			{
+				email_address: email,
+				status: 'subscribed',
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName,
+				},
+			},
+		],
+	};
 
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: 'subscribed',
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName
-        }
-      }
-    ]
-  }
+	var jsonData = JSON.stringify(data);
 
-  var jsonData = JSON.stringify(data);
+	const url = process.env.MAILCHIMP_URI;
 
-  const url = "https://us17.api.mailchimp.com/3.0/lists/63ea3abc6f"
+	const option = {
+		method: 'POST',
+		auth: process.env.MAILCHIMP_AUTH,
+	};
 
-  const option = {
-    method: "POST",
-    auth: "sajjad:665b783bf8ddfef38c1510b412f3b930-us17"
-    //Mailchimp requires us to put ANY username prior to the auth key
-  }
+	const request = https.request(url, option, (response) => {
+		response.on('data', (data) => {
+			const errorCount = JSON.parse(data).error_count;
 
-  const request = https.request(url, option, (response) => {
+			if (response.statusCode === 200 && errorCount === 0) {
+				res.sendFile(__dirname + '/success.html');
+			} else {
+				res.sendFile(__dirname + '/failure.html');
+			}
+		});
+	});
 
-    response.on("data", (data) => {
-      const errorCount = JSON.parse(data).error_count;
-
-      if (response.statusCode === 200 && errorCount === 0) {
-        res.sendFile(__dirname + '/success.html');
-      } else {
-        res.sendFile(__dirname + '/failure.html');
-
-      }
-
-      // We log here to get the data response back from the server of Mailchimp to 
-      // see everything worked well
-    })
-  }) //After searching online, Angela found that assigning the request to a variable
-  // and then writing to it, and then ending the data is the way to go
-
-  request.write(jsonData);
-  request.end();
-
-  // Data to send to the user
+	request.write(jsonData);
+	request.end();
 });
 
 app.post('/failure', (req, res) => {
-  res.redirect('/');
-})
+	res.redirect('/');
+});
 
-app.listen(process.env.PORT || 3000, () => console.log('Server running at port 3000'));
+app.listen(process.env.PORT || 3000, () =>
+	console.log('Server running at port 3000'),
+);
