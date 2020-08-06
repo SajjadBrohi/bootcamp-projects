b176e5a19bd4331ddc609a719132a575cd313fa0
SajjadBrohi <s.brohi@uq.net.au>
2020-08-06 21:33:10 +1000

    Add environment variables to hide API key

--- Sec20 - API/weather-app/app.js
+++ Sec20 - API/weather-app/app.js
@@ -1,42 +1,59 @@
 const express = require('express');
 const https = require('https');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', (req, res) => {
-  const cityName = req.body.cityName;
-  const apiKey = "9f5922f82d0ba2ed132cc16c148b8caf"
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=metric";
+	const cityName = req.body.cityName;
+	const apiKey = process.env.WEATHER_API_KEY;
+	const url =
+		'https://api.openweathermap.org/data/2.5/weather?q=' +
+		cityName +
+		'&appid=' +
+		apiKey +
+		'&units=metric';
 
-  const options = {
-    method: "GET",
-    auth: url
-  }
+	const options = {
+		method: 'GET',
+		auth: url,
+	};
 
-  const request = https.request(url, options, (response) => {
+	const request = https.request(url, options, (response) => {
+		console.log(response.statusCode);
+		response.on('data', (data) => {
+			const weatherData = JSON.parse(data);
+			// console.log(weatherData);
+			const temp = weatherData.main.temp;
+			const tempDescription = weatherData.weather[0].description;
+			const icon = weatherData.weather[0].icon;
 
-    console.log(response.statusCode)
-    response.on('data', (data) => {
-      const weatherData = JSON.parse(data);
-      // console.log(weatherData);
-      const temp = weatherData.main.temp;
-      const tempDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
+			res.write(
+				'<h1>The weather in ' +
+					cityName +
+					' is ' +
+					temp +
+					'</h1>\
+        <h1>The weather in ' +
+					cityName +
+					' is like ' +
+					tempDescription +
+					'</h1> ',
+			);
+			res.write(
+				'<img src=http://openweathermap.org/img/wn/' + icon + '@2x.png>',
+			);
+			res.send();
+		});
+	});
 
-      res.write("<h1>The weather in " + cityName + " is " + temp + "</h1>\
-        <h1>The weather in " + cityName + " is like " + tempDescription + "</h1> ");
-      res.write("<img src=http://openweathermap.org/img/wn/" + icon + "@2x.png>");
-      res.send();
-    })
-  });
-
-  request.end();
-})
+	request.end();
+});
 
 app.listen(3000, () => console.log('Server is running on port 3000.'));
